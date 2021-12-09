8ddf7150b334b49c7c7b71d27a24b6eb24102f91
lipa <marcin.lipa@onet.pl>
2021-12-09 20:47:28 +0100

    extract const values

---
+++ frontend/src/app/core/const/consts.ts
@@ -0,0 +1,18 @@
+import {environment} from "../../../environments/environment";
+
+export const routes = {
+  home: "/home",
+  register: "/register",
+  discover: "/discover",
+  followed: "/followed",
+  profile: "/profile",
+  instagram: "#",
+  facebook: "#",
+  twitter: "#"
+};
+
+export const endpoints = {
+  discover: `${environment.apiUrl}/discover`,
+  login: `${environment.apiUrl}/login`,
+
+};
