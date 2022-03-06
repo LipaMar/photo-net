3d0c0b730c340024b5d2b65517af7f53269d8b72
lipa <marcin.lipa@onet.pl>
2022-03-06 14:05:19 +0100

    number of likes - follow tab

--- frontend/src/app/pages/followed/followed.component.ts
+++ frontend/src/app/pages/followed/followed.component.ts
@@ -10,76 +10,77 @@ import {IMasonryGalleryImage} from "ngx-masonry-gallery";
 import {ImageUtils} from "../../core/utils/ImageUtils";
 
 @Component({
-    selector: 'app-followed',
-    templateUrl: './followed.component.html',
-    styleUrls: ['./followed.component.scss'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-followed',
+  templateUrl: './followed.component.html',
+  styleUrls: ['./followed.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FollowedComponent implements OnInit, OnDestroy {
-    routeToDiscover = routes.discover;
-    routeToProfile = routes.profile + '/';
-    posts: PostDisplay[];
-    images: IMasonryGalleryImage[];
-    subscriptions = new SubscriptionContainer();
-    profilePics = new Map<string, any>();
+  routeToDiscover = routes.discover;
+  routeToProfile = routes.profile + '/';
+  posts: PostDisplay[];
+  images: IMasonryGalleryImage[];
+  subscriptions = new SubscriptionContainer();
+  profilePics = new Map<string, any>();
 
-    constructor(private postService: PostService,
-                private profileService: ProfileService,
-                private datePipe: DatePipe) {
-    }
+  constructor(private postService: PostService,
+              private profileService: ProfileService,
+              private datePipe: DatePipe) {
+  }
 
-    ngOnInit(): void {
-        this.subscriptions.add = this.postService.getPostsOfFollowedUsers().subscribe(posts => {
-            this.posts = posts;
-            this.images = ImageUtils.postsToPostImage(this.posts);
-            this.posts.map(post => post.timestamp = this.transform(post.timestamp));
-            this.posts.map(post => post.author).filter((value, index, self) => self.indexOf(value) === index).forEach(author => {
-                this.profilePics.set(author, null);
-                this.subscriptions.add = this.profileService.getProfileSimple(author).subscribe(profile => {
-                    this.profilePics.set(profile.userName, profile.profilePicture);
-                });
-            });
-            this.subscriptions.add = this.postService.getLiked().subscribe(liked => this.setLikedPropertyForAllPosts(liked));
+  ngOnInit(): void {
+    this.subscriptions.add = this.postService.getPostsOfFollowedUsers().subscribe(posts => {
+      this.posts = posts;
+      this.images = ImageUtils.postsToPostImage(this.posts);
+      this.posts.map(post => post.timestamp = this.transform(post.timestamp));
+      this.posts.map(post => post.author).filter((value, index, self) => self.indexOf(value) === index).forEach(author => {
+        this.profilePics.set(author, null);
+        this.subscriptions.add = this.profileService.getProfileSimple(author).subscribe(profile => {
+          this.profilePics.set(profile.userName, profile.profilePicture);
         });
-    }
+      });
+      this.subscriptions.add = this.postService.getLiked().subscribe(liked => this.setLikedPropertyForAllPosts(liked));
+    });
+  }
 
-    private setLikedPropertyForAllPosts(ids: number[]) {
-        this.posts.forEach(post => {
-            post.liked = ids.indexOf(post.id) >= 0;
-        });
-    }
+  private setLikedPropertyForAllPosts(ids: number[]) {
+    this.posts.forEach(post => {
+      post.liked = ids.indexOf(post.id) >= 0;
+    });
+  }
 
-    private transform(timestamp: string) {
-        const transform = this.datePipe.transform(timestamp, DatePattern.TIME_DATE_DOTS);
-        return transform ? transform : "";
-    }
+  private transform(timestamp: string) {
+    const transform = this.datePipe.transform(timestamp, DatePattern.TIME_DATE_DOTS);
+    return transform ? transform : "";
+  }
 
-    showProfilePic(author: string) {
-        const url = this.profilePics.get(author);
-        return url == null ? "assets/images/anon.svg" : url;
-    }
+  showProfilePic(author: string) {
+    const url = this.profilePics.get(author);
+    return url == null ? "assets/images/anon.svg" : url;
+  }
 
-    styleLikeButtons(likeButton: HTMLButtonElement, post: PostDisplay) {
-        const item = likeButton.children.item(0);
-        if (item) {
-            if (post.liked) {
-                item.className = "fas fa-thumbs-up";
-                return `btn btn-dark-brown`;
-            } else {
-                item.className = "far fa-thumbs-up";
-                return `btn btn-outline-dark-brown`;
-            }
-        }
-        return "btn btn-light";
+  styleLikeButtons(likeButton: HTMLButtonElement, post: PostDisplay) {
+    const item = likeButton.children.item(0);
+    if (item) {
+      if (post.liked) {
+        item.className = "fas fa-thumbs-up";
+        return `btn btn-dark-brown`;
+      } else {
+        item.className = "far fa-thumbs-up";
+        return `btn btn-outline-dark-brown`;
+      }
     }
+    return "btn btn-light";
+  }
 
-    likeClicked(post: PostDisplay) {
-        post.liked = !post.liked;
-        this.subscriptions.add = this.postService.like(post.id).subscribe();
-    }
+  likeClicked(post: PostDisplay) {
+    post.liked = !post.liked;
+    this.subscriptions.add = this.postService.like(post.id).subscribe();
+    post.liked ? post.likes++ : post.likes--;
+  }
 
-    ngOnDestroy(): void {
-        this.subscriptions.dispose();
-    }
+  ngOnDestroy(): void {
+    this.subscriptions.dispose();
+  }
 
 }
